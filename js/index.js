73f9356cc3373c332710903a04434c4abe04613e
Emilia312CQP <chenqiuping1993@163.com>
2020-03-02 14:21:04 +0800

    重写首页样式，引用了jquery动画函数、css3的动画及动画函数

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(function () {
+    $('#nav>.wrapper>div>div').on('mouseover', function () {
+        $(this).find('ul').fadeIn('slow');
+        $(this).find('.icon').addClass('animated shake fast').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+            $(this).removeClass('animated shake  fast')
+        });
+    });
+    $('#nav>.wrapper>div>div>ul').on('mouseover', function (e) {
+        /*阻止事件冒泡*/
+        e.stopPropagation();
+
+
+    });
+    $('ul').on('mouseleave', function ( ) {
+      $(this).css('display','none');
+
+    })
+
+
+});
